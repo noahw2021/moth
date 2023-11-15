9c833306d2bf084687faf4eaa7c925d5bfb144e4
noahw2021 <noah.wooten@icloud.com>
2023-11-15 06:27:34 -0500

    create first file, add pccel as submodule

---
+++ moth/main.c
@@ -0,0 +1,15 @@
+/*
+main.c
+moth
+15 November 2023
+
+The Moth Operating System
+(c) Noah Wooten 2023, All Rights Reserved
+
+Changes:
+11/15/23 : File created - nw
+*/
+
+int main(void) {
+	return 0;
+}
