141f8cfafc6886fb33db7f36046b5622f379511d
noahw2021 <noah.wooten@icloud.com>
2023-11-15 07:49:27 -0500

    update file header

--- moth/main.c
+++ moth/main.c
@@ -6,6 +6,9 @@ moth
 The Moth Operating System
 (c) Noah Wooten 2023, All Rights Reserved
 
+This file is licensed under the MothOSL.
+https://github.com/noahw2021/moth/blob/master/LICENSE
+
 Changes:
 11/15/23 : File created - nw
 */
